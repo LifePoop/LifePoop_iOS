a325361d76884b32d0afb55df788e05507244765
sanghyeok-kim <slsnsi1212@gmail.com>
2023-04-26 15:53:31 +0900

    #1 chore: SwiftLintRunScript.sh 프로젝트 루트 디렉토리 수정 (상대위치 -> 절대위치)

--- Scripts/SwiftLintRunScript.sh
+++ Scripts/SwiftLintRunScript.sh
@@ -5,7 +5,8 @@ fi
 export PATH
 
 if which swiftlint > /dev/null; then
-    swiftlint --config ../../.swiftlint.yml --quiet
+    project_root=$(git rev-parse --show-toplevel)
+    swiftlint --config "${project_root}/.swiftlint.yml" --quiet
 else
     echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
 fi

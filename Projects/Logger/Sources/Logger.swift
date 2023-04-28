9c0ef62d2441818ee7ca9ae8e40c81c4376a464b
sanghyeok-kim <slsnsi1212@gmail.com>
2023-04-28 22:16:48 +0900

    #2 feat: Logger 모듈 기본 코드 구현

--- Projects/Logger/Sources/Logger.swift
+++ Projects/Logger/Sources/Logger.swift
@@ -1,8 +1,26 @@
 //
 //  Logger.swift
-//  LoggerManifests
+//  Logger
 //
-//  Created by 김상혁 on 2023/04/26.
+//  Created by 김상혁 on 2023/04/27.
+//  Copyright © 2023 LifePoop. All rights reserved.
 //
 
 import Foundation
+import OSLog
+
+public struct Logger {
+    public static func log(message: String, category: OSLog.LogCategory, type: OSLogType = .error) {
+        #if DEBUG
+        let log = OSLog(subsystem: OSLog.subsystem, category: category.value)
+        os_log(type, log: log, "\(message)")
+        #endif
+    }
+    
+    public static func logDeallocation<T: AnyObject>(object: T) {
+        #if DEBUG
+        let log = OSLog(subsystem: OSLog.subsystem, category: OSLog.LogCategory.allocation.value)
+        os_log(.info, log: log, "\(T.self) deallocated")
+        #endif
+    }
+}
---
+++ Projects/Logger/Sources/Rx+logErrorIfDetected.swift
@@ -0,0 +1,34 @@
+//
+//  Rx+logErrorIfDetected.swift
+//  Logger
+//
+//  Created by 김상혁 on 2023/04/28.
+//  Copyright © 2023 LifePoop. All rights reserved.
+//
+
+import OSLog
+
+import RxSwift
+
+public extension ObservableType {
+    func logErrorIfDetected() -> Observable<Element> {
+        return self.catch { error -> Observable<Element> in
+            let errorMessage = error.localizedDescription
+            let errorCategory = (error as? OSLoggable)?.category ?? .default
+            Logger.log(message: errorMessage, category: errorCategory, type: .error)
+            return Observable.error(error)
+        }
+    }
+}
+
+public extension PrimitiveSequence where Trait == SingleTrait {
+    func logErrorIfDetected() -> Single<Element> {
+        return self.asObservable().logErrorIfDetected().asSingle()
+    }
+}
+
+public extension PrimitiveSequence where Trait == CompletableTrait, Element == Never {
+    func logErrorIfDetected() -> Completable {
+        return self.asObservable().logErrorIfDetected().asCompletable()
+    }
+}

da17eb08e2da84f3d3664f25d8e1ed2be76c3f62
sanghyeok-kim <slsnsi1212@gmail.com>
2023-05-04 11:31:38 +0900

    #10 feat: ProfileImageView 구현

---
+++ Projects/Features/FeatureHome/FeatureHomePresentation/Sources/ProfileImageView.swift
@@ -0,0 +1,41 @@
+//
+//  ProfileImageView.swift
+//  FeatureHomePresentation
+//
+//  Created by 김상혁 on 2023/05/01.
+//  Copyright © 2023 LifePoop. All rights reserved.
+//
+
+import UIKit
+
+public final class ProfileImageView: CircularImageView {
+    
+    private let activeImage = UIImage(
+        named: "profileActivated",
+        in: Bundle(identifier: "LifePoop.DesignSystem"), compatibleWith: nil
+    )?.withRenderingMode(.alwaysOriginal)
+    
+    private let deActiveImage = UIImage(
+        named: "profileDeactivated",
+        in: Bundle(identifier: "LifePoop.DesignSystem"), compatibleWith: nil
+    )?.withRenderingMode(.alwaysOriginal)
+    
+    public init() {
+        super.init(frame: .zero)
+        contentMode = .scaleAspectFit
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public func setImage(by isActivated: Bool) {
+        switch isActivated {
+        case true:
+            image = activeImage
+        case false:
+            image = deActiveImage
+        }
+    }
+}

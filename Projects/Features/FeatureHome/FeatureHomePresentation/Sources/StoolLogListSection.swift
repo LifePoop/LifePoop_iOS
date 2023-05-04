c1f43b7359b5e217d6d9214f88b9c71eb51286b2
sanghyeok-kim <slsnsi1212@gmail.com>
2023-05-04 11:30:07 +0900

    #10 feat: StoolLogListSection 구현

---
+++ Projects/Features/FeatureHome/FeatureHomePresentation/Sources/StoolLogListSection.swift
@@ -0,0 +1,13 @@
+//
+//  StoolLogListSection.swift
+//  FeatureHomePresentation
+//
+//  Created by 김상혁 on 2023/05/03.
+//  Copyright © 2023 LifePoop. All rights reserved.
+//
+
+import Foundation
+
+public enum StoolLogListSection {
+    case log
+}

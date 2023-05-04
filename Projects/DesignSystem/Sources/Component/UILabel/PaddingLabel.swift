d1674157f0645e2376c31cca5e0741a90a74b26b
sanghyeok-kim <slsnsi1212@gmail.com>
2023-05-04 16:18:03 +0900

    #12 move: DesignSystem 모듈 폴더링(Component, Enum, Extension), UI Component 코드 이동

--- Projects/DesignSystem/Sources/Component/UILabel/PaddingLabel.swift
+++ Projects/DesignSystem/Sources/Component/UILabel/PaddingLabel.swift
@@ -1,19 +1,17 @@
 //
-//  PaddingButton.swift
+//  PaddingLabel.swift
 //  FeatureHomePresentation
 //
-//  Created by 김상혁 on 2023/05/02.
+//  Created by 김상혁 on 2023/05/03.
 //  Copyright © 2023 LifePoop. All rights reserved.
 //
 
 import UIKit
 
-import DesignSystem // FIXME: DesignSystem 모듈로 이동할 경우 삭제
-
-public class PaddingButton: UIButton {
+public class PaddingLabel: UILabel {
     
     private let padding: UIEdgeInsets
-
+    
     public override var intrinsicContentSize: CGSize {
         var contentSize = super.intrinsicContentSize
         if contentSize == .zero { return contentSize }
@@ -21,14 +19,18 @@ public class PaddingButton: UIButton {
         contentSize.width += padding.left + padding.right
         return contentSize
     }
-
+    
     public init(padding: Padding = .medium) {
         self.padding = padding.insets
         super.init(frame: .zero)
     }
-
+    
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override func drawText(in rect: CGRect) {
+        super.drawText(in: rect.inset(by: padding))
     }
 }

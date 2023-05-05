cd42bcadb4c202936ebbcd0d0b3128db721d0b09
sanghyeok-kim <slsnsi1212@gmail.com>
2023-05-05 18:47:50 +0900

    #17 feat: DesignSystemAsset에 대한 ImageAsset, ColorAsset Typealias 구현

---
+++ Projects/DesignSystem/Sources/Asset/Typealias/ImageAsset.swift
@@ -0,0 +1,9 @@
+//
+//  ImageAsset.swift
+//  DesignSystem
+//
+//  Created by 김상혁 on 2023/05/05.
+//  Copyright © 2023 LifePoop. All rights reserved.
+//
+
+public typealias ImageAsset = DesignSystemAsset.Image

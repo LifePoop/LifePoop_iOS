97968d77f3510e4fbc808aa9e18dc7b62514a73c
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2023-05-07 03:04:17 +0900

    #14 feat: 기록 화면에 사용할 커스텀 셀 구현
    - 색상, 강도, 크기 선택에 대해 각각 ColorSelectionCell, StiffnessSelectionCell, SizeSelectionCell 구현

---
+++ Projects/Features/FeatureStoolLog/FeatureStoolLogPresentation/Sources/Views/SatisfactionDetail/DetailSelection/SizeSelectionCell.swift
@@ -0,0 +1,53 @@
+//
+//  SizeSelectionCell.swift
+//  FeatureStoolLogPresentation
+//
+//  Created by 이준우 on 2023/05/06.
+//  Copyright © 2023 LifePoop. All rights reserved.
+//
+
+import UIKit
+
+import SnapKit
+
+import CoreEntity
+
+import DesignSystem
+
+
+final class SizeSelectionCell: UICollectionViewCell {
+        
+    private let imageView: UIImageView = UIImageView(image: ImageAsset.sizeSelectMedium.original)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubViews()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(sizeSelection: SelectableSize) {
+        
+        var image: UIImage
+        switch sizeSelection {
+        case .small:
+            image = ImageAsset.sizeSelectSmall.image
+        case .medium:
+            image = ImageAsset.sizeSelectMedium.image
+        case .large:
+            image = ImageAsset.sizeSelectLarge.image
+        }
+        imageView.image = image
+    }
+    
+    private func addSubViews() {
+        contentView.addSubview(imageView)
+        imageView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+    }
+}

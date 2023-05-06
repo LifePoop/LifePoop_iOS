0acf248738f90c4e4f43f3b0a3482205f25ff8dd
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2023-05-07 03:00:45 +0900

    #14 feat: StoolLogCoordinator 및 관련된 필요타입 구현

---
+++ Projects/Features/FeatureStoolLog/FeatureStoolLogPresentation/Sources/Coordinator/StoolLogCoordinator.swift
@@ -0,0 +1,14 @@
+//
+//  StoolLogCoordinator.swift
+//  FeatureStoolLogPresentation
+//
+//  Created by 이준우 on 2023/05/05.
+//  Copyright © 2023 LifePoop. All rights reserved.
+//
+
+import Foundation
+import Utils
+
+public protocol StoolLogCoordinator: Coordinator {
+    func coordinate(by coordinateAction: StoolLogCoordinateAction)
+}

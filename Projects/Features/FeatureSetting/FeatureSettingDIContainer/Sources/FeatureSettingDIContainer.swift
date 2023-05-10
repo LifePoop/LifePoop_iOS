174006c65feff7ae92eac2dc349a71bb1049b48d
sanghyeok-kim <slsnsi1212@gmail.com>
2023-05-10 13:52:05 +0900

    #20 feat: FeatureSetting의 각 모듈별 Sources, Tests 디렉토리와 플레이스 홀더 파일 생성

---
+++ Projects/Features/FeatureSetting/FeatureSettingDIContainer/Sources/FeatureSettingDIContainer.swift
@@ -0,0 +1,8 @@
+//
+//  FeatureSettingDIContainer.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by 김상혁 on 2023/05/10.
+//
+
+import Foundation
---
+++ Projects/Features/FeatureSetting/FeatureSettingPresentation/Sources/FeatureSettingPresentation.swift
@@ -0,0 +1,8 @@
+//
+//  FeatureSettingPresentation.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by 김상혁 on 2023/05/10.
+//
+
+import Foundation

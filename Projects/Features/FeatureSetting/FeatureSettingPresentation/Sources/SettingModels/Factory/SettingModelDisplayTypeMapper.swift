af25496e748d46678963e1c4a8d82e56f78cc56e
sanghyeok-kim <slsnsi1212@gmail.com>
2023-05-23 12:57:14 +0900

    #20 refactor: SettingModel로부터 분리된 Section, DisplayType에 대한 책임을 갖는 별도의 Mapper 구현

---
+++ Projects/Features/FeatureSetting/FeatureSettingPresentation/Sources/SettingModels/Factory/SettingModelDisplayTypeMapper.swift
@@ -0,0 +1,26 @@
+//
+//  SettingModelDisplayTypeMapper.swift
+//  FeatureSettingPresentation
+//
+//  Created by 김상혁 on 2023/05/23.
+//  Copyright © 2023 LifePoop. All rights reserved.
+//
+
+import Foundation
+
+struct SettingModelDisplayTypeMapper {
+    static func mapDisplayType(for model: SettingModel) -> SettingInfoDisplayType {
+        switch model.type {
+        case .loginType:
+            return .loginType
+        case .profile:
+            return .textTap
+        case .autoLogin:
+            return .`switch`
+        case .version:
+            return .text
+        case .termsOfService, .privacyPolicy, .sendFeedback:
+            return .tap
+        }
+    }
+}

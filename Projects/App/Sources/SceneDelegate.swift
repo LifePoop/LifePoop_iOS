982be9e490582639c5f3b0f745232e286cb0a4cd
sanghyeok-kim <slsnsi1212@gmail.com>
2023-05-27 23:23:15 +0900

    #43 refactor: NavigationBar BackButtonIndicator를 빈 이미지로 설정시 발생하는 레이아웃 경고 처리를 위해 이미지 크기 설정

--- Projects/App/Sources/SceneDelegate.swift
+++ Projects/App/Sources/SceneDelegate.swift
@@ -75,7 +75,7 @@ private extension SceneDelegate {
 
 private extension SceneDelegate {
     func configureNavigationBarBackButtonItem() {
-        let emptyImage = UIImage()
+        let emptyImage = UIGraphicsImageRenderer(size: CGSize(width: 1, height: 1)).image { _ in }
         UINavigationBar.appearance().backIndicatorImage = emptyImage
         UINavigationBar.appearance().backIndicatorTransitionMaskImage = emptyImage
     }

9d74ed0ed4c36fbfdf9996af11defbe15e148264
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2023-05-30 01:03:07 +0900

    #38 feat: KakaoAuthManager 구현체 정의

---
+++ Projects/Core/CoreAuthentication/Sources/Managers/KakaoAuthManager.swift
@@ -0,0 +1,78 @@
+//
+//  KakaoAuthManager.swift
+//  Auth
+//
+//  Created by 이준우 on 2023/05/29.
+//  Copyright © 2023 Lifepoo. All rights reserved.
+//
+
+import Foundation
+
+import CoreEntity
+
+import KakaoSDKAuth
+import KakaoSDKCommon
+import KakaoSDKUser
+import RxSwift
+
+public final class KakaoAuthManager: AuthManagable {
+
+    public static internal(set) var isAlreadyInitialized: Bool = false
+    
+    public init() { }
+    
+    public static func initAuthInfo(rightAfter preparation: (() -> Void)?) {
+        guard !isAlreadyInitialized else { return }
+        
+        preparation?()
+
+        let serialQueue = DispatchQueue(label: "serial-queue-kakao-auth")
+        serialQueue.sync {
+            // FIXME: 추후 키값 코드에서 감춰놔야 함
+            KakaoSDK.initSDK(appKey: "f7f327d46b7184823676acc9d0a2035c")
+            
+            isAlreadyInitialized = true
+        }
+    }
+    
+    public static func handleLoginUrl(_ url: URL) {
+        if AuthApi.isKakaoTalkLoginUrl(url) {
+            _ = AuthController.handleOpenUrl(url: url)
+        }
+    }
+        
+    public func fetchToken() -> Single<AuthResultPossessable> {
+        Single.create { observer in
+            guard KakaoAuthManager.isAlreadyInitialized else {
+                observer(.failure(LoginError.authInfoNotInitialized))
+                return Disposables.create { }
+            }
+            
+            guard UserApi.isKakaoTalkLoginAvailable() else {
+                observer(.failure(LoginError.kakaoTalkLoginNotAvailable))
+                return Disposables.create { }
+            }
+            
+            UserApi.shared.loginWithKakaoTalk(completion: { token, error in
+                if let error = error {
+                    observer(.failure(error))
+                    return
+                }
+                
+                guard let token = token else {
+                    observer(.failure(LoginError.authTokenNil))
+                    return
+                }
+                
+                let authResultEntity = KakaoAuthResultEntity(
+                    accessToken: token.accessToken,
+                    refreshToken: token.refreshToken
+                )
+                
+                observer(.success(authResultEntity))
+            })
+            
+            return Disposables.create()
+        }
+    }
+}

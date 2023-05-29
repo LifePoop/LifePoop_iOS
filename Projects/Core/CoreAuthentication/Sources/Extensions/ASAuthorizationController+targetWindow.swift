758d787180b71ad0539dc634872e06a388810c96
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2023-05-30 01:03:42 +0900

    #38 feat: AppleAuthManager 구현체 정의, 필요한 rx extension, delegate proxy 구현

---
+++ Projects/Core/CoreAuthentication/Sources/Extensions/ASAuthorizationController+targetWindow.swift
@@ -0,0 +1,15 @@
+//
+//  ASAuthorizationController+targetWindow.swift
+//  CoreAuthentication
+//
+//  Created by 이준우 on 2023/05/29.
+//  Copyright © 2023 Lifepoo. All rights reserved.
+//
+
+import AuthenticationServices
+
+extension ASAuthorizationController {
+    
+    public static var targetWindow: UIWindow?
+    
+}

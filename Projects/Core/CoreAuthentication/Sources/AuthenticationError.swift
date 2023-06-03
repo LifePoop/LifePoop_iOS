feaf1add314bd300cf586f3abe3393d56bc40ffe
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2023-06-04 01:55:06 +0900

    #38 refactor: AuthenticationError에 LocalizedError 채택

--- Projects/Core/CoreAuthentication/Sources/AuthenticationError.swift
+++ Projects/Core/CoreAuthentication/Sources/AuthenticationError.swift
@@ -8,8 +8,19 @@
 
 import Foundation
 
-public enum AuthenticationError: Error {
+public enum AuthenticationError: LocalizedError {
     case authInfoNotInitialized
     case kakaoTalkLoginNotAvailable
     case authTokenNil
+    
+    public var errorDescription: String? {
+        switch self {
+        case .authInfoNotInitialized:
+            return "인증 정보가 초기화되지 않았습니다."
+        case .kakaoTalkLoginNotAvailable:
+            return "카카오 로그인이 현재 불가합니다."
+        case .authTokenNil:
+            return "인증 토큰이 존재하지 않습니다.(nil value)"
+        }
+    }
 }

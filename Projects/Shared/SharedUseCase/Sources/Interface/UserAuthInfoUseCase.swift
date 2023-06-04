daa9b45b3f9080519eeef657ce29d77926ac6194
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2023-06-04 23:44:22 +0900

    #45 refactor: LoginTypeUseCase -> UserAuthInfoUseCase 타입명 , 하위 속성 타입 수정
    - 키체인에서 인증 정보 자체를 가져옴에 따라 이름 및, 하위 속성 타입을 수정함

---
+++ Projects/Shared/SharedUseCase/Sources/Interface/UserAuthInfoUseCase.swift
@@ -0,0 +1,20 @@
+//
+//  UserAuthInfoUseCase.swift
+//  SharedUseCase
+//
+//  Created by 김상혁 on 2023/05/27.
+//  Copyright © 2023 Lifepoo. All rights reserved.
+//
+
+import Foundation
+
+import RxSwift
+
+import CoreEntity
+
+public protocol UserAuthInfoUseCase {
+    var userAuthInfo: Single<UserAuthInfo> { get }
+
+    // TODO: KeyChain에서 인증정보 가져오기 때문에 해당 함수 수정 혹은 삭제 여부 확인 필요
+    func updateLoginType(to newLoginType: LoginType)
+}

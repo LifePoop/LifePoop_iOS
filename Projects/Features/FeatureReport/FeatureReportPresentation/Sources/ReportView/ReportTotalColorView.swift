7c3d818c0830f13c51747b84ca19a62c8ee0d94c
sanghyeok-kim <slsnsi1212@gmail.com>
2023-06-08 12:23:34 +0900

    #58 Report 화면의 '총 배변 횟수, 만족도, 색깔, 모양, 크기' 커스텀 뷰 UI 구현

---
+++ Projects/Features/FeatureReport/FeatureReportPresentation/Sources/ReportView/ReportTotalColorView.swift
@@ -0,0 +1,39 @@
+//
+//  ReportTotalColorView.swift
+//  FeatureReportPresentation
+//
+//  Created by 김상혁 on 2023/06/08.
+//  Copyright © 2023 Lifepoo. All rights reserved.
+//
+
+import UIKit
+
+import RxCocoa
+import RxSwift
+import SnapKit
+
+import DesignSystem
+import Utils
+
+public final class ReportTotalColorView: UIView {
+    
+    private let barView = StoolCountBarView(color: ColorAsset.pooBrown.color, barWidthPercentage: 1, count: 5)
+    
+    public init() {
+        super.init(frame: .zero)
+        layoutUI()
+    }
+    
+    @available(*, unavailable)
+    public required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func layoutUI() {
+        addSubview(barView)
+        
+        barView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}

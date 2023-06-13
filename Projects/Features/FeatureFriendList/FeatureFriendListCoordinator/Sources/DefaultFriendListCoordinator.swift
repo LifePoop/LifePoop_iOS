f8df92af936112f59ed7272bc3f263be964adf28
junu0516 <junu0516@kakao.com>
2023-06-13 19:35:55 +0900

    #61 feat: DefaultFriendListCoordinator의 화면흐름 구현, 기타 필요한 작업 처리
    - SceneDeleagte에서 필요한 의존성 DIContainer에 등록
    - LaunchScreenViewController이 LifepoopViewController 상속받도록 처리

--- Projects/Features/FeatureFriendList/FeatureFriendListCoordinator/Sources/DefaultFriendListCoordinator.swift
+++ Projects/Features/FeatureFriendList/FeatureFriendListCoordinator/Sources/DefaultFriendListCoordinator.swift
@@ -16,7 +16,7 @@ import Utils
 
 public final class DefaultFriendListCoordinator: FriendListCoordinator {
 
-    public var childCoordinatorMap: [CoordinatorType : Coordinator] = [:]
+    public var childCoordinatorMap: [CoordinatorType: Coordinator] = [:]
     public var navigationController: UINavigationController
     public let type: CoordinatorType = .friendList
     
@@ -27,16 +27,39 @@ public final class DefaultFriendListCoordinator: FriendListCoordinator {
     }
     
     public func start() {
-        
+        coordinate(by: .shouldShowFirendList)
     }
     
     public func coordinate(by coordinateAction: FriendListCoordinateAction) {
-        
+        DispatchQueue.main.async { [weak self] in
+            switch coordinateAction {
+            case .shouldShowFirendList:
+                self?.showFriendListViewController()
+            case .shouldShowFriendInvitation:
+                self?.showFriendInvitationView()
+            }
+        }
+    }
+}
+
+private extension DefaultFriendListCoordinator {
+    
+    func showFriendListViewController() {
+        let viewModel = FriendListViewModel(coordinator: self)
+        let viewController = FriendListViewController()
+        viewController.bind(viewModel: viewModel)
+        navigationController.pushViewController(viewController, animated: true)
     }
     
-    
-    
-    
-    
-    
+    func showFriendInvitationView() {
+        
+        let invitationViewController = FrinedInvitationViewController()
+        let invitationViewModel = FriendInvitationViewModel()
+        invitationViewController.bind(viewModel: invitationViewModel)
+        
+        let parentViewController = navigationController
+        let bottomSheetController = BottomSheetController(bottomSheetHeight: 255)
+        bottomSheetController.setBottomSheet(contentViewController: invitationViewController)
+        bottomSheetController.showBottomSheet(toParent: parentViewController)
+    }
 }

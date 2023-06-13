34fb74aaf4104f2ed82fd6f2088c347bcbf0257b
sanghyeok-kim <slsnsi1212@gmail.com>
2023-06-13 19:13:45 +0900

    #58 feat: ReportViewModel에 NicknameUseCase 의존성 추가 및 사용자 닉네임 불러오는 로직 구현

--- Projects/Features/FeatureReport/FeatureReportPresentation/Sources/ReportViewModel.swift
+++ Projects/Features/FeatureReport/FeatureReportPresentation/Sources/ReportViewModel.swift
@@ -13,6 +13,8 @@ import CoreEntity
 import FeatureReportCoordinatorInterface
 import FeatureReportDIContainer
 import FeatureReportUseCase
+import SharedDIContainer
+import SharedUseCase
 import Utils
 
 public final class ReportViewModel: ViewModelType {
@@ -22,16 +24,30 @@ public final class ReportViewModel: ViewModelType {
     }
     
     public struct Output {
+        let updateUserNickname = PublishRelay<String>()
         let showErrorMessage = PublishRelay<String>()
     }
     
     public let input = Input()
     public let output = Output()
     
+    @Inject(SharedDIContainer.shared) private var nicknameUseCase: NicknameUseCase
     private weak var coordinator: ReportCoordinator?
     private let disposeBag = DisposeBag()
     
     public init(coordinator: ReportCoordinator?) {
         self.coordinator = coordinator
+        let fetchedUserNickname = input.viewDidLoad
+            .withUnretained(self)
+            .flatMapMaterialized { `self`, _ in
+                self.nicknameUseCase.nickname
+            }
+            .share()
+        
+        fetchedUserNickname
+            .compactMap { $0.element }
+            .compactMap { $0 }
+            .bind(to: output.updateUserNickname)
+            .disposed(by: disposeBag)
     }
 }

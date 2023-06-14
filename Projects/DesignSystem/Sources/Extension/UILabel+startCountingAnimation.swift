3720d45bdefb5b3cfb4fe01524b7e550420ee801
sanghyeok-kim <slsnsi1212@gmail.com>
2023-06-14 21:45:24 +0900

    #58 refactor: UILabel+startCountingAnimation 메소드 매개변수를 클로저로 받아 실행해주는 방식으로 변경, 그에 따른 주석 수정

--- Projects/DesignSystem/Sources/Extension/UILabel+startCountingAnimation.swift
+++ Projects/DesignSystem/Sources/Extension/UILabel+startCountingAnimation.swift
@@ -15,32 +15,42 @@ public extension UILabel {
     
     /// Starts the counting animation for the label.
     /// - Parameters:
-    ///   - count: The target count for the animation. Must be greater than 0.
-    ///   - duration: The duration of the animation in seconds. Must be greater than 0. Default is 0.75.
-    ///   - suffix: The suffix to append to the animated count. Default is an empty string.
+    ///   - targetCount: The final count for the animation. Must be greater than or equal to 0.
+    ///   - duration: The duration of the animation in seconds. Must be greater than 0. Default is 0.5.
+    ///   - configureTextAction: A closure that is invoked on each animation step with the current count value.
     ///
     /// - Important:
-    ///   - The `count` parameter must be greater than 0.
+    ///   - The `targetCount` parameter must be equal to or greater than 0.
     ///   - The `duration` parameter must be greater than 0.
-    func startCountingAnimation(count: Int, duration: Double = 0.5, suffix: String = "") {
+    func startCountingAnimation(
+        upTo targetCount: Int,
+        duration: Double = 0.5,
+        configureTextAction: @escaping (_ animatingCount: Int) -> Void
+    ) {
         guard duration > 0 else {
             Logger.log(message: "duration은 0보다 커야합니다.", category: .default, type: .error)
             return
         }
-        guard count > 0 else {
-            Logger.log(message: "count는 0보다 커야합니다.", category: .default, type: .error)
+        
+        guard targetCount >= 0 else {
+            Logger.log(message: "count는 0보다 크거나 같아야합니다.", category: .default, type: .error)
+            return
+        }
+        
+        if targetCount == 0 {
+            configureTextAction(targetCount)
             return
         }
         
         let initialCount: Double = 0.0
-        let finalCount: Double = Double(count)
+        let finalCount: Double = Double(targetCount)
         let numberOfSteps: Int = 100
         let timeInterval = duration / Double(numberOfSteps)
         let countIncrement = finalCount - initialCount
         for step in 0...numberOfSteps {
             DispatchQueue.main.asyncAfter(deadline: .now() + timeInterval * Double(step)) {
                 let currentCount = Int(initialCount + countIncrement * (Double(step) / Double(numberOfSteps)))
-                self.text = "\(currentCount)\(suffix)"
+                configureTextAction(currentCount)
             }
         }
     }

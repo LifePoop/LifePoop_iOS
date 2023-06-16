7b95348befc56e569caf1af2a021f76da000ab79
sanghyeok-kim <slsnsi1212@gmail.com>
2023-06-16 15:39:21 +0900

    #67 feat: NicknameInputState, BirthdayTextInput에 대한 descriptionText extension 추가

---
+++ Projects/EntityUIMapper/Sources/BirthdayTextInput+descriptionText.swift
@@ -0,0 +1,27 @@
+//
+//  BirthdayTextInput+descriptionText.swift
+//  EntityUIMapper
+//
+//  Created by 김상혁 on 2023/06/16.
+//  Copyright © 2023 Lifepoo. All rights reserved.
+//
+
+import Foundation
+
+import CoreEntity
+import DesignSystem
+
+public extension BirthdayTextInput.Status {
+    var descriptionText: ConditionalTextField.TextFieldStatus {
+        switch self {
+        case .`default`:
+            return ConditionalTextField.TextFieldStatus.none(text: "생년월일 6자를 입력해주세요.")
+        case .defaultWarning:
+            return ConditionalTextField.TextFieldStatus.impossible(text: "생년월일 6자를 입력해주세요.")
+        case .possible:
+            return ConditionalTextField.TextFieldStatus.possible(text: "유효한 생년월일 형식입니다.")
+        case .impossible:
+            return ConditionalTextField.TextFieldStatus.impossible(text: "유효하지 않은 생년월일 형식입니다.")
+        }
+    }
+}
---
+++ Projects/EntityUIMapper/Sources/NicknameInput+descriptionText.swift
@@ -0,0 +1,27 @@
+//
+//  NicknameTextInput+descriptionText.swift
+//  EntityUIMapper
+//
+//  Created by 김상혁 on 2023/06/16.
+//  Copyright © 2023 Lifepoo. All rights reserved.
+//
+
+import Foundation
+
+import CoreEntity
+import DesignSystem
+
+public extension NicknameTextInput.Status {
+    var descriptionText: ConditionalTextField.TextFieldStatus {
+        switch self {
+        case .`default`:
+            return ConditionalTextField.TextFieldStatus.none(text: "2~5자로 한글, 영문, 숫자를 사용할 수 있습니다.")
+        case .defaultWarning:
+            return ConditionalTextField.TextFieldStatus.impossible(text: "2~5자로 한글, 영문, 숫자를 사용할 수 있습니다.")
+        case .possible:
+            return ConditionalTextField.TextFieldStatus.possible(text: "사용 가능한 닉네임입니다.")
+        case .impossible:
+            return ConditionalTextField.TextFieldStatus.impossible(text: "사용 불가능한 닉네임입니다.")
+        }
+    }
+}

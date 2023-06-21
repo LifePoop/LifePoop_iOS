82d3204ababfe239cb206eb1dd5cb4b989dcf455
sanghyeok-kim <slsnsi1212@gmail.com>
2023-06-21 21:22:57 +0900

    #70 refactor: FriendEntity, StoolLogEntity의 dummyData 값 수정

--- Projects/Core/CoreEntity/Sources/FriendEntity.swift
+++ Projects/Core/CoreEntity/Sources/FriendEntity.swift
@@ -25,20 +25,21 @@ extension FriendEntity: Hashable { }
 extension FriendEntity {
     public static var dummyData: [FriendEntity] {
         return [
-            FriendEntity(name: "김상혁", isActivated: true, profile: .init(color: .yellow, shape: .good)),
-            FriendEntity(name: "김상혁가가", isActivated: false, profile: .init(color: .brown, shape: .soft)),
-            FriendEntity(name: "김상혁나나", isActivated: false, profile: .init(color: .yellow, shape: .good)),
-            FriendEntity(name: "김상혁다다", isActivated: false, profile: .init(color: .brown, shape: .good)),
-            FriendEntity(name: "손혜정가가", isActivated: true, profile: .init(color: .green, shape: .soft)),
-            FriendEntity(name: "손혜정나나나", isActivated: false, profile: .init(color: .pink, shape: .good)),
-            FriendEntity(name: "손혜정다다다", isActivated: false, profile: .init(color: .black, shape: .good)),
-            FriendEntity(name: "이준우가가가", isActivated: false, profile: .init(color: .yellow, shape: .hard)),
-            FriendEntity(name: "박서원", isActivated: true, profile: .init(color: .brown, shape: .hard)),
-            FriendEntity(name: "김유빈", isActivated: false, profile: .init(color: .green, shape: .good)),
-            FriendEntity(name: "이화정", isActivated: true, profile: .init(color: .black, shape: .soft)),
-            FriendEntity(name: "김상혁11", isActivated: false, profile: .init(color: .yellow, shape: .good)),
-            FriendEntity(name: "김상혁123", isActivated: false, profile: .init(color: .pink, shape: .soft)),
-            FriendEntity(name: "김상혁123", isActivated: true, profile: .init(color: .yellow, shape: .good))
+            FriendEntity(name: "김유빈", isActivated: true, profile: .init(color: .brown, shape: .soft)),
+            FriendEntity(name: "이화정", isActivated: false, profile: .init(color: .brown, shape: .good)),
+            FriendEntity(name: "이준우", isActivated: true, profile: .init(color: .brown, shape: .hard)),
+            FriendEntity(name: "강시온", isActivated: false, profile: .init(color: .black, shape: .soft)),
+            FriendEntity(name: "손혜정", isActivated: true, profile: .init(color: .black, shape: .good)),
+            FriendEntity(name: "김상혁", isActivated: false, profile: .init(color: .black, shape: .hard)),
+            FriendEntity(name: "강시온가나", isActivated: true, profile: .init(color: .pink, shape: .soft)),
+            FriendEntity(name: "김상혁다라", isActivated: false, profile: .init(color: .pink, shape: .good)),
+            FriendEntity(name: "손혜정마바", isActivated: true, profile: .init(color: .pink, shape: .hard)),
+            FriendEntity(name: "김유빈사아", isActivated: false, profile: .init(color: .green, shape: .soft)),
+            FriendEntity(name: "이준우자차", isActivated: true, profile: .init(color: .green, shape: .good)),
+            FriendEntity(name: "이화정카타", isActivated: false, profile: .init(color: .green, shape: .hard)),
+            FriendEntity(name: "라이푸12", isActivated: false, profile: .init(color: .yellow, shape: .soft)),
+            FriendEntity(name: "LFPOO", isActivated: true, profile: .init(color: .yellow, shape: .good)),
+            FriendEntity(name: "12345", isActivated: true, profile: .init(color: .yellow, shape: .hard))
         ]
     }
 }

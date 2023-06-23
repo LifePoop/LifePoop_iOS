c223a39f894ffaad5afe48abb9994ae9ea8b85f5
sanghyeok-kim <slsnsi1212@gmail.com>
2023-06-23 20:00:42 +0900

    #82 refactor: ProfileEditUseCase의 updateProfileInfo 메소드 요청이 성공하도록 수정

--- Projects/Features/FeatureSetting/FeatureSettingUseCase/Sources/MockProfileEditUseCase.swift
+++ Projects/Features/FeatureSetting/FeatureSettingUseCase/Sources/MockProfileEditUseCase.swift
@@ -15,17 +15,6 @@ import SharedDIContainer
 import SharedUseCase
 import Utils
 
-enum MockError: LocalizedError {
-    case failToEditProfile
-    
-    var errorDescription: String? {
-        switch self {
-        case .failToEditProfile:
-            return "Profile Edit Error (Mock)"
-        }
-    }
-}
-
 public protocol ProfileEditUseCase {
     func updateProfileInfo(newProfileCharacter: ProfileCharacter, newNickname: String) -> Completable
     func checkNicknameValidation(for input: String) -> Observable<NicknameTextInput>
@@ -48,21 +37,10 @@ public final class MockProfileEditUseCase: ProfileEditUseCase {
     }
     
     public func updateProfileInfo(newProfileCharacter: ProfileCharacter, newNickname: String) -> Completable {
-        // Success
-//         return Completable.zip(
-//             nicknameUseCase.updateNickname(to: newNickname),
-//             profileCharacterUseCase.updateProfileCharacter(to: newProfileCharacter)
-//         )
-//         .delay(.milliseconds(1000), scheduler: MainScheduler.asyncInstance)
-        
-//         Failure
-         let errorAfterOneSecond: Completable = Completable.create { completable in
-             DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-                 completable(.error(MockError.failToEditProfile))
-             }
-             return Disposables.create()
-         }
-         return errorAfterOneSecond.logErrorIfDetected(category: .network)
-        
+         return Completable.zip(
+             nicknameUseCase.updateNickname(to: newNickname),
+             profileCharacterUseCase.updateProfileCharacter(to: newProfileCharacter)
+         )
+         .delay(.milliseconds(1000), scheduler: MainScheduler.asyncInstance) // FIXME: 서버 통신시 delay 삭제
     }
 }

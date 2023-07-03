205710f043e3257d36ac1d55c5fce7a57e3c8d9a
junu0516 <junu0516@kakao.com>
2023-07-04 01:44:42 +0900

    #84 feat: Segment의 track, untrack 표시방식 수정

--- Projects/DesignSystem/Sources/Component/SegmentedProgressView/Segment.swift
+++ Projects/DesignSystem/Sources/Component/SegmentedProgressView/Segment.swift
@@ -10,9 +10,22 @@ import UIKit
 
 final class Segment: UIView {
     
+    private let trackedMark: UIView = {
+        let view = UIView()
+        view.backgroundColor = ColorAsset.primary.color
+        return view
+    }()
+
+    private let unTrackedmark: UIView = {
+        let view = UIView()
+        view.backgroundColor = ColorAsset.white.color
+        return view
+    }()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         configureUI()
+        layoutUI()
     }
     
     required public init?(coder: NSCoder) {
@@ -24,11 +37,33 @@ final class Segment: UIView {
         layer.cornerRadius = 2.5
     }
     
-    func setTracked() {
-        backgroundColor = ColorAsset.white.color
+    private func layoutUI() {
+        
+        let segmentWidth = frame.width
+        let segmentHeight = frame.height
+        
+        unTrackedmark.frame = CGRect(origin: .zero, size: .init(width: segmentWidth, height: segmentHeight))
+        trackedMark.frame = CGRect(origin: .zero, size: .init(width: .zero, height: segmentHeight))
+        
+        addSubview(unTrackedmark)
+        addSubview(trackedMark)
+    }
+    
+    func setTracked(animated: Bool = false, completion: (() -> Void)? = nil) {
+        if isAlreadyTracked() { return }
+
+        let segmentWidth = frame.width
+
+        trackedMark.frame.size.width = segmentWidth
     }
     
     func setUntracked() {
-        backgroundColor = ColorAsset.primary.color
+        guard isAlreadyTracked() else { return }
+        
+        trackedMark.frame.size.width = .zero
+    }
+    
+    private func isAlreadyTracked() -> Bool {
+        trackedMark.frame.size.width > .zero
     }
 }

4e9c89faed9ce0d031e0863c5536b5452fb9ac92
junu0516 <junu0516@kakao.com>
2023-07-09 21:38:27 +0900

    #84 chore: 스토리 시간차 확인을 위해 임시로 StoolLogEntity 더미데이터의 기록 시간 수정

--- Projects/Core/CoreEntity/Sources/StoolLogEntity.swift
+++ Projects/Core/CoreEntity/Sources/StoolLogEntity.swift
@@ -38,21 +38,21 @@ extension StoolLogEntity: Hashable { }
 extension StoolLogEntity {
     public static var dummyData: [StoolLogEntity] {
         return [
-            StoolLogEntity(id: 0, date: "오후 11:41", isSatisfied: true, color: .brown, shape: .soft, size: .small),
-            StoolLogEntity(id: 1, date: "오후 11:42", isSatisfied: false, color: .brown, shape: .good, size: .small),
-            StoolLogEntity(id: 2, date: "오후 11:43", isSatisfied: true, color: .brown, shape: .hard, size: .small),
-            StoolLogEntity(id: 3, date: "오후 11:44", isSatisfied: false, color: .black, shape: .soft, size: .small),
-            StoolLogEntity(id: 4, date: "오후 11:45", isSatisfied: true, color: .black, shape: .good, size: .small),
-            StoolLogEntity(id: 5, date: "오후 11:46", isSatisfied: true, color: .black, shape: .hard, size: .small),
-            StoolLogEntity(id: 6, date: "오후 11:47", isSatisfied: true, color: .pink, shape: .soft, size: .small),
-            StoolLogEntity(id: 7, date: "오후 11:48", isSatisfied: true, color: .pink, shape: .good, size: .small),
-            StoolLogEntity(id: 8, date: "오후 11:49", isSatisfied: true, color: .pink, shape: .hard, size: .small),
-            StoolLogEntity(id: 9, date: "오후 11:50", isSatisfied: true, color: .green, shape: .soft, size: .small),
-            StoolLogEntity(id: 10, date: "오후 11:51", isSatisfied: true, color: .green, shape: .good, size: .small),
-            StoolLogEntity(id: 11, date: "오후 11:52", isSatisfied: true, color: .green, shape: .hard, size: .small),
-            StoolLogEntity(id: 12, date: "오후 11:53", isSatisfied: false, color: .yellow, shape: .soft, size: .small),
-            StoolLogEntity(id: 13, date: "오후 11:54", isSatisfied: true, color: .yellow, shape: .good, size: .small),
-            StoolLogEntity(id: 14, date: "오후 11:55", isSatisfied: false, color: .yellow, shape: .hard, size: .small)
+            StoolLogEntity(id: 0, date: "오전 11:41", isSatisfied: true, color: .brown, shape: .soft, size: .small),
+            StoolLogEntity(id: 1, date: "오전 11:42", isSatisfied: false, color: .brown, shape: .good, size: .small),
+            StoolLogEntity(id: 2, date: "오전 11:43", isSatisfied: true, color: .brown, shape: .hard, size: .small),
+            StoolLogEntity(id: 3, date: "오후 12:44", isSatisfied: false, color: .black, shape: .soft, size: .small),
+            StoolLogEntity(id: 4, date: "오후 12:45", isSatisfied: true, color: .black, shape: .good, size: .small),
+            StoolLogEntity(id: 5, date: "오후 12:46", isSatisfied: true, color: .black, shape: .hard, size: .small),
+            StoolLogEntity(id: 6, date: "오후 12:47", isSatisfied: true, color: .pink, shape: .soft, size: .small),
+            StoolLogEntity(id: 7, date: "오후 13:48", isSatisfied: true, color: .pink, shape: .good, size: .small),
+            StoolLogEntity(id: 8, date: "오후 15:49", isSatisfied: true, color: .pink, shape: .hard, size: .small),
+            StoolLogEntity(id: 9, date: "오후 16:50", isSatisfied: true, color: .green, shape: .soft, size: .small),
+            StoolLogEntity(id: 10, date: "오후 18:51", isSatisfied: true, color: .green, shape: .good, size: .small),
+            StoolLogEntity(id: 11, date: "오후 20:52", isSatisfied: true, color: .green, shape: .hard, size: .small),
+            StoolLogEntity(id: 12, date: "오후 21:53", isSatisfied: false, color: .yellow, shape: .soft, size: .small),
+            StoolLogEntity(id: 13, date: "오후 22:54", isSatisfied: true, color: .yellow, shape: .good, size: .small),
+            StoolLogEntity(id: 14, date: "오후 23:55", isSatisfied: false, color: .yellow, shape: .hard, size: .small)
         ]
     }
 }

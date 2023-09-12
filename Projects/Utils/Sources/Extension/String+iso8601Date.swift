fab502f85cfec6d112ff1af4f323225f12f0f2bc
sanghyeok-kim <slsnsi1212@gmail.com>
2023-09-12 10:30:38 +0900

    #112 chore: String, Date 전환을 위한 extension에 주석 추가

--- Projects/Utils/Sources/Extension/String+iso8601Date.swift
+++ Projects/Utils/Sources/Extension/String+iso8601Date.swift
@@ -9,6 +9,10 @@
 import Foundation
 
 public extension String {
+    /// ISO8601 형식의 `String` 타입을 `Date?` 타입으로 변환합니다.
+    /// ISO8601 형식이 아닐 경우, `nil`을 반환합니다.
+    ///
+    /// 예) "2023-09-10T15:30:00Z"
     var iso8601Date: Date? {
         return LifePoopDateFormatter.shared.convertISO8601FormatStringToDate(from: self)
     }

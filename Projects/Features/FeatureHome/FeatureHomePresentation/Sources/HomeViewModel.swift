8841ba9c7ccf4bedb75e0fa5ee4246cfda18d84e
sanghyeok-kim <slsnsi1212@gmail.com>
2023-09-20 23:39:05 +0900

    Revert "#103 remove: HomeViewModel의 불필요한 이벤트 처리 로직 제거"
    This reverts commit e74d4a083183a5738a8e60af76bc5b55901994a3.

--- Projects/Features/FeatureHome/FeatureHomePresentation/Sources/HomeViewModel.swift
+++ Projects/Features/FeatureHome/FeatureHomePresentation/Sources/HomeViewModel.swift
@@ -20,6 +20,7 @@ public final class HomeViewModel: ViewModelType {
     
     public struct Input {
         let viewDidLoad = PublishRelay<Void>()
+        let viewWillAppear = PublishRelay<Void>()
         let viewDidRefresh = PublishRelay<Void>() // TODO: 로직 구현 필요
         let settingButtonDidTap = PublishRelay<Void>()
         let reportButtonDidTap = PublishRelay<Void>()
@@ -36,6 +37,7 @@ public final class HomeViewModel: ViewModelType {
     }
     
     public struct State {
+        let userProfileCharacter = BehaviorRelay<FriendEntity?>(value: nil)
         let friends = BehaviorRelay<[FriendEntity]>(value: [])
         let stoolLogs = BehaviorRelay<[StoolLogEntity]>(value: [])
         let headerViewModel = BehaviorRelay<StoolLogHeaderViewModel?>(value: nil)
@@ -134,8 +136,6 @@ public final class HomeViewModel: ViewModelType {
             }
             .disposed(by: disposeBag)
         
-        // MARK: - Bind State
-        
         state.friends
             .map { $0.isEmpty }
             .distinctUntilChanged()
@@ -160,6 +160,17 @@ public final class HomeViewModel: ViewModelType {
             .bind(to: output.updateStoolLogs)
             .disposed(by: disposeBag)
         
+        state.stoolLogs
+            .map { !$0.isEmpty }
+            .withLatestFrom(state.userProfileCharacter) { ($0, $1) }
+            .compactMap { (isStoolLogEmpty, userProfileCharacter) in
+                var newProfileCharacter = userProfileCharacter
+                newProfileCharacter?.isActivated = isStoolLogEmpty
+                return newProfileCharacter
+            }
+            .bind(to: state.userProfileCharacter)
+            .disposed(by: disposeBag)
+        
         state.headerViewModel
             .compactMap { $0 }
             .bind(to: output.bindStoolLogHeaderViewModel)
@@ -175,6 +186,10 @@ private extension HomeViewModel {
     func bind(stoolLogHeaderViewModel: StoolLogHeaderViewModel) {
         input.viewDidRefresh
             .bind(to: stoolLogHeaderViewModel.input.viewDidRefresh)
+            .disposed(by: disposeBag)
+        
+        state.userProfileCharacter
+            .bind(to: stoolLogHeaderViewModel.state.userProfileCharacter)
             .disposed(by: disposeBag)
         
         state.friends

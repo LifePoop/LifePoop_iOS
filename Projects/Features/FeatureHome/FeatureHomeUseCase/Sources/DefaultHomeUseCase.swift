7d010856094e02738adb45fdb8c2d24bcbe5642b
sanghyeok-kim <slsnsi1212@gmail.com>
2023-10-31 23:50:33 +0900

    #128 feat: HomeUseCase에 StoryFeedUseCase 연결, 불필요한 코드 제거

--- Projects/Features/FeatureHome/FeatureHomeUseCase/Sources/DefaultHomeUseCase.swift
+++ Projects/Features/FeatureHome/FeatureHomeUseCase/Sources/DefaultHomeUseCase.swift
@@ -21,10 +21,15 @@ public final class DefaultHomeUseCase: HomeUseCase {
     
     @Inject(SharedDIContainer.shared) private var userInfoUseCase: UserInfoUseCase
     @Inject(SharedDIContainer.shared) private var cheeringInfoUseCase: CheeringInfoUseCase
+    @Inject(SharedDIContainer.shared) private var storyFeedUseCase: StoryFeedUseCase
     @Inject(SharedDIContainer.shared) private var stoolLogUseCase: StoolLogUseCase
     @Inject(HomeDIContainer.shared) private var friendListRepository: HomeRepository
     
     public init() { }
+    
+    public func fetchStoryFeeds() -> Observable<[StoryFeedEntity]> {
+        return storyFeedUseCase.fetchStoryFeeds()
+    }
     
     public func fetchCheeringInfo(at date: String) -> Observable<CheeringInfoEntity> {
         return userInfoUseCase.userInfo
@@ -53,36 +58,11 @@ public final class DefaultHomeUseCase: HomeUseCase {
             .fetchMyLast7DaysStoolLogs()
     }
     
-    // FIXME: 선택된 친구의 스토리 불러오는 코드 - 삭제 또는 수정 필요
-    public func fetchStoolLogsOfSelectedFriend(_ friend: FriendEntity) -> Observable<[StoolStoryLogEntity]> {
-        return stoolLogUseCase
-            .fetchAllUserStoolLogs(userID: friend.id)
-            .withUnretained(self)
-            .compactMap { `self`, friendStoolLogs in
-                self.convertToStoryLogs(friendStoolLogs)
-            }
-    }
-    
     public func postStoolLog(_ stoolLogEntity: StoolLogEntity) -> Observable<StoolLogEntity> {
         return stoolLogUseCase.postStoolLog(stoolLogEntity: stoolLogEntity)
     }
     
     public func convertToStoolLogItems(from stoolLogsEntities: [StoolLogEntity]) -> [StoolLogItem] {
         return stoolLogUseCase.convertToStoolLogItems(from: stoolLogsEntities)
-    }
-}
-
-private extension DefaultHomeUseCase {
-    // FIXME: 선택된 친구의 스토리 불러오는 코드 - 삭제 또는 수정 필요
-    func convertToStoryLogs(_ stoolLogs: [StoolLogEntity]) -> [StoolStoryLogEntity] {
-        let sortedStoolLogs = stoolLogs.sorted { $0.date < $1.date }
-        // TODO: 추후 서버에서 DTO 내려줄 때, '힘주기' 여부가 포함되야 하고, 아래 isFirst 조건과 함께 고려해서 isCheeringUpAvailable값이 결정되야 함
-        // 우선은 isLast여부로만 힘주기 가능여부 판단하도록 처리
-        let storyLogs = sortedStoolLogs.enumerated().map { index, stoolLog in
-            let isLast = index == stoolLogs.count-1
-            return StoolStoryLogEntity(stoolLog: stoolLog, isCheeringUpAvailable: isLast)
-        }
-        
-        return storyLogs
     }
 }

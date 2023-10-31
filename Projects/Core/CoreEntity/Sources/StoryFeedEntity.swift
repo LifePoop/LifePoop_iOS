0b430474ff58737b1af2257e63df53cbc726c185
sanghyeok-kim <slsnsi1212@gmail.com>
2023-10-31 23:40:58 +0900

    #128 feat: StoryFeedEntity, StoryEntity 구현

---
+++ Projects/Core/CoreEntity/Sources/StoryFeedEntity.swift
@@ -0,0 +1,31 @@
+//
+//  StoryFeed.swift
+//  CoreEntity
+//
+//  Created by 김상혁 on 2023/10/31.
+//  Copyright © 2023 Lifepoo. All rights reserved.
+//
+
+import Foundation
+
+public struct StoryFeedEntity {
+    public let hassId: UUID = UUID()
+    
+    public let user: CheeringFriendInfoEntity
+    public let stories: [StoryEntity]
+    
+    public init(user: CheeringFriendInfoEntity, stories: [StoryEntity]) {
+        self.user = user
+        self.stories = stories
+    }
+}
+
+extension StoryFeedEntity: Hashable {
+    public static func == (lhs: Self, rhs: Self) -> Bool {
+        return lhs.hassId == rhs.hassId
+    }
+    
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(hassId)
+    }
+}

16bf674986f901e6dbf2b3c10ea7b7ef95703e36
Junu Lee(Jed) <junu0516@kakao.com>
2023-11-11 21:20:24 +0900

    #148 feat: Rx+log 추가

---
+++ Projects/Logger/Sources/Rx+log.swift
@@ -0,0 +1,38 @@
+//
+//  Rx+log.swift
+//  Logger
+//
+//  Created by Lee, Joon Woo on 2023/11/11.
+//  Copyright © 2023 Lifepoo. All rights reserved.
+//
+
+import Foundation
+import OSLog
+
+import RxSwift
+
+public extension ObservableType {
+    func log(message: String, category: OSLog.LogCategory, printElement: Bool = false) -> Observable<Element> {
+        return self.logErrorIfDetected(category: category, type: .error)
+            .do(onNext: { element in
+            let meesage = printElement ? "\(message)\nvalue:\(element)" : message
+            Logger.log(message: message, category: category, type: .debug)
+        })
+    }
+}
+
+public extension PrimitiveSequence where Trait == SingleTrait {
+    func log(message: String, category: OSLog.LogCategory, printElement: Bool = false) -> Single<Element> {
+        return self.asObservable()
+            .log(message: message, category: category, printElement: printElement)
+            .asSingle()
+    }
+}
+
+public extension PrimitiveSequence where Trait == CompletableTrait, Element == Never {
+    func log(message: String, category: OSLog.LogCategory, printElement: Bool = false) -> Completable {
+        return self.asObservable()
+            .log(message: message, category: category, printElement: printElement)
+            .asCompletable()
+    }
+}
